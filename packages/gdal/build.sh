b1e8855539154e62d535dbea183539bf2cb85886
Henrik Grimler <henrik@grimler.se>
2019-07-31 07:29:28 +0200

    gdal: conflict with dev pkg and add libc++ as dependency
    %ci-no-build

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -6,6 +6,8 @@ TERMUX_PKG_LICENSE_FILE="LICENSE.TXT"
 TERMUX_PKG_VERSION=3.0.0
 TERMUX_PKG_SRCURL=https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ad316fa052d94d9606e90b20a514b92b2dd64e3142dfdbd8f10981a5fcd5c43e
-TERMUX_PKG_DEPENDS="openjpeg, libcurl, libtiff, libpng, proj, libiconv, libsqlite, libgeos"
+TERMUX_PKG_DEPENDS="libc++, openjpeg, libcurl, libtiff, libpng, proj, libiconv, libsqlite, libgeos"
+TERMUX_PKG_BREAKS="gdal-dev"
+TERMUX_PKG_REPLACES="gdal-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-sqlite3=$TERMUX_PREFIX"
