49ac6d4673c3d3b1d79d5df49c178c0df14ca48c
Henrik Grimler <henrik@grimler.se>
2019-09-20 22:14:28 +0200

    gdal: bump revision
    Build seem to work after termux-packages submodule is updated

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="Bjoern Schilberg @BjoernSchilberg"
 TERMUX_PKG_VERSION=3.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=45b4ae25dbd87282d589eca76481c426f72132d7a599556470d5c38263b09266
 TERMUX_PKG_DEPENDS="libc++, openjpeg, libcurl, libtiff, libpng, proj, libiconv, libsqlite, libgeos, libexpat"
@@ -11,7 +12,3 @@ TERMUX_PKG_BREAKS="gdal-dev"
 TERMUX_PKG_REPLACES="gdal-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-sqlite3=$TERMUX_PREFIX"
-
-termux_step_pre_configure() {
-	CXXFLAGS+=" -DDOXYGEN_SKIP"
-}
