c2c163a8102eb51142a3daa560a93e2c5de6a2ed
Henrik Grimler <henrik@grimler.se>
2020-07-12 09:15:11 +0200

    openfoam: update to v2012

--- packages/openfoam/build.sh
+++ packages/openfoam/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.openfoam.com
 TERMUX_PKG_DESCRIPTION="OpenFOAM is a CFD software written in C++"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1906
-TERMUX_PKG_SRCURL=https://sourceforge.net/projects/openfoamplus/files/v$TERMUX_PKG_VERSION/OpenFOAM-v$TERMUX_PKG_VERSION.tgz
-TERMUX_PKG_SHA256=15e38c2dc659b63753a0dd3dff913222cc46d6a40089a1b76973dd741145f61a
-TERMUX_PKG_DEPENDS="openmpi, flex, boost, libc++"
+TERMUX_PKG_VERSION=2012
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/openfoam/files/v2012/OpenFOAM-v$TERMUX_PKG_VERSION.tgz
+TERMUX_PKG_SHA256=3d6e39e39e7ae61d321fbc6db6c3748e6e5e1c4886454207a7f1a7321469e65a
+TERMUX_PKG_DEPENDS="openmpi, flex, boost, cgal, libc++"
 TERMUX_PKG_RM_AFTER_INSTALL="opt/OpenFOAM-v${TERMUX_PKG_VERSION}/build"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
@@ -21,26 +21,26 @@ termux_step_host_build() {
 		set -u
 		make
 	)
-	mkdir -p wmake/platforms
-	mv $TERMUX_PKG_SRCDIR/wmake/platforms/linuxGcc wmake/platforms/
-	mv $TERMUX_PKG_SRCDIR/wmake/platforms/linux64Gcc wmake/platforms/
+	mkdir -p platforms/tools
+	mv $TERMUX_PKG_SRCDIR/platforms/tools/linuxGcc platforms/tools/
+	mv $TERMUX_PKG_SRCDIR/platforms/tools/linux64Gcc platforms/tools/
 }
 
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH" == "aarch64" ]; then
-		ARCH_FOLDER=linuxARM64Clang80
+		ARCH_FOLDER=linuxARM64Clang
 		TERMUX_COMPILER_PREFIX="aarch64-linux-android"
 		export ARCH=aarch64
 	elif [ "$TERMUX_ARCH" == "arm" ]; then
-		ARCH_FOLDER=linuxARM7Clang80
+		ARCH_FOLDER=linuxARM7Clang
 		TERMUX_COMPILER_PREFIX="arm-linux-androideabi"
 		export ARCH=armv7l
 	elif [ "$TERMUX_ARCH" == "i686" ]; then
-		ARCH_FOLDER=linuxClang80
+		ARCH_FOLDER=linuxClang
 		TERMUX_COMPILER_PREFIX="i686-linux-android"
 		export ARCH=i686
 	elif [ "$TERMUX_ARCH" == "x86_64" ]; then
-		ARCH_FOLDER=linux64Clang80
+		ARCH_FOLDER=linux64Clang
 		TERMUX_COMPILER_PREFIX="x86_64-linux-android"
 		export ARCH=x86_64
 	fi
@@ -51,12 +51,12 @@ termux_step_pre_configure() {
 	set +u
 	source "$TERMUX_PKG_SRCDIR/etc/bashrc" || true
 	set -u
-	mkdir -p wmake/platforms
-	cp -r $TERMUX_PKG_HOSTBUILD_DIR/wmake/platforms/linux64Gcc wmake/platforms/${ARCH_FOLDER}
+	mkdir -p platforms/tools
+	cp -r $TERMUX_PKG_HOSTBUILD_DIR/platforms/tools/linux64Gcc platforms/tools/${ARCH_FOLDER}
 	if [ $TERMUX_ARCH_BITS = 32 ]; then
-		cp -r $TERMUX_PKG_HOSTBUILD_DIR/wmake/platforms/linuxGcc wmake/platforms/${ARCH_FOLDER}
+		cp -r $TERMUX_PKG_HOSTBUILD_DIR/platforms/tools/linuxGcc platforms/tools/${ARCH_FOLDER}
 	else
-		cp -r $TERMUX_PKG_HOSTBUILD_DIR/wmake/platforms/linux64Gcc wmake/platforms/${ARCH_FOLDER}
+		cp -r $TERMUX_PKG_HOSTBUILD_DIR/platforms/tools/linux64Gcc platforms/tools/${ARCH_FOLDER}
 	fi
 }
 
@@ -69,10 +69,10 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/opt
-	cp -r $TERMUX_PKG_SRCDIR $TERMUX_PREFIX/opt/OpenFOAM-v${TERMUX_PKG_VERSION}
+	mkdir -p $TERMUX_PKG_MASSAGEDIR$TERMUX_PREFIX/opt
+	cp -r $TERMUX_PKG_SRCDIR $TERMUX_PKG_MASSAGEDIR$TERMUX_PREFIX/opt/OpenFOAM-v${TERMUX_PKG_VERSION}
 }
 
 termux_step_post_make_install() {
-	sed -i 's%$ARCH%$(uname -m)%g' $TERMUX_PREFIX/opt/OpenFOAM-v${TERMUX_PKG_VERSION}/etc/config.sh/settings
+	sed -i 's%$ARCH%$(uname -m)%g' $TERMUX_PKG_MASSAGEDIR$TERMUX_PREFIX/opt/OpenFOAM-v${TERMUX_PKG_VERSION}/etc/config.sh/settings
 }
